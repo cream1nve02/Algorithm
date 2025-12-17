63c37e6b4aaee27b7f4b5e7df50c345c976b5113
Chaemin Park <cream1nve@gmail.com>
2025-12-17 12:55:58 +0900

    queue

---
+++ 3st_week/03_10_queue.py
@@ -0,0 +1,56 @@
+from operator import is_not
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+#head -> tail
+#[4] -> [3]
+
+#[4] -> [3] -> [2]
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        self.tail.next = new_node
+        self.tail = new_node
+        return
+
+    def dequeue(self):
+        if self.is_empty():
+            return "Queue is empty"
+        delete_head = self.head
+        self.head = self.head.next
+        return delete_head
+
+    def peek(self):
+        if self.is_empty():
+            return "Queue is empty"
+        return self.head.data
+
+    def is_empty(self):
+        return self.head is None
+
+
+
+queue = Queue()
+queue.enqueue(4)
+print(queue.peek())
+queue.enqueue(2)
+print(queue.peek())
+queue.enqueue(3)
+print(queue.peek())
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+
+print(queue.peek())
