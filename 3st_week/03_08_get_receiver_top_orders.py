dc817f12469fa4dd6bba1c0b0db64a62d048e936
Chaemin Park <cream1nve@gmail.com>
2025-12-17 12:15:22 +0900

    stack example

--- 3st_week/03_08_get_receiver_top_orders.py
+++ 3st_week/03_08_get_receiver_top_orders.py
@@ -10,8 +10,8 @@ top_heights = [6, 9, 5, 7, 4]
 
 def get_receiver_top_orders(heights):
     answer = [0] * len(heights) # 아무 것도 못 받으면 0으로 반환!
-    for i in range(len(heights) - 1, 0, -1):
-        for j in range(i - 1, -1, -1):
+    for i in range(len(heights) - 1, 0, -1):  # O(N)
+        for j in range(i - 1, -1, -1): # O(N)
             if heights[i] <= heights[j]:
                answer[i] = j + 1
                break
--- 3st_week/03_08_get_receiver_top_orders_stack.py
+++ 3st_week/03_08_get_receiver_top_orders_stack.py
@@ -11,11 +11,12 @@ top_heights = [6, 9, 5, 7, 4]
 def get_receiver_top_orders(heights):
     answer = [0] * len(heights) # 아무 것도 못 받으면 0으로 반환!
 
+    # O(N)
     while heights: # 해당 stack이 비어있지 않다면 계속 반복해라.
         height = heights.pop()
         #            0  1  2  3
         # heights = [6, 9, 5, 7]
-        for i in range(len(heights) - 1, -1, -1):
+        for i in range(len(heights) - 1, -1, -1): # O(N)
             if height <= heights[i]:
                 answer[len(heights)] = i + 1
                 break
